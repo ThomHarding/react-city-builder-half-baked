6670f57d4c3da674b8651d641aaf4857ca60157c
THOMAS HARDING <hardingt604@gmail.com>
2022-05-02 16:12:29 -0600

    corrected capitalisation of ids in app.js (ID to Id)

--- src/App.js
+++ src/App.js
@@ -11,9 +11,9 @@ import City from './City';
 function App() {
   // track some state here.
   // You'll need to keep track of a skylineId, waterfrontId, and castleId. All these start out as 1
-  const [skylineID, setSkyline] = useState(1);
-  const [waterfrontID, setWaterfront] = useState(1);
-  const [castleID, setCastle] = useState(1);
+  const [skylineId, setSkyline] = useState(1);
+  const [waterfrontId, setWaterfront] = useState(1);
+  const [castleId, setCastle] = useState(1);
   // you'll need to track a city name, which starts as the city name of your choice.
   const [cityName, setCityName] = useState('Billiamshire');
   // finally, you'll need an array of slogans, which could start out as ['The City of Excellence'] for example
@@ -21,8 +21,8 @@ function App() {
 
   return (
     <div className="App">
-      {/* here, the City component takes in skylineId, waterfrontId, castleId as props. It'll use those ids to render pictures correctly. */}
-      <City skylineID={skylineID} waterfrontID={waterfrontID} castleID={castleID} />
+      {/* here, the City component takes in skylineId, waterfrontId, castleId as props. It'll use those Ids to render pictures correctly. */}
+      <City skylineId={skylineId} waterfrontId={waterfrontId} castleId={castleId} />
       <h1>
         {/* dynamically update the city name here using state */}
         Welcome to beautiful {cityName}!
