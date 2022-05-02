b814b7a24d9f34da42cf4030522fa900190d5d05
THOMAS HARDING <hardingt604@gmail.com>
2022-05-02 16:10:48 -0600

    corrected file name for dropdown selected images

--- src/City.js
+++ src/City.js
@@ -5,9 +5,9 @@ export default function City(props) {
     <div className="city">
       {/* this component should take 3 props: waterfrontId, skylineId, and castleId */}
       {/* use these 3 ids in img tags load appropriate images that live in the public directory */}
-      <img src={`${props.waterfrontId}.png`}/>
-      <img src={`${props.skylineId}.png`}/>
-      <img src={`${props.castleId}.png`}/>
+      <img src={`waterfront-${props.waterfrontId}.jpeg`}/>
+      <img src={`skyline-${props.skylineId}.jpeg`}/>
+      <img src={`castle-${props.castleId}.jpeg`}/>
     </div>
   );
 }
