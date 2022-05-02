f54271706bd661d9e0a0780b790457a6f1361c23
THOMAS HARDING <hardingt604@gmail.com>
2022-05-02 15:34:08 -0600

    add citynameinput and slogan.js (rendering at most basic form

--- src/Slogan.js
+++ src/Slogan.js
@@ -4,6 +4,7 @@ export default function Slogan(props) {
   return (
     <div className='slogan'>
       {/* this component takes in a slogan as a prop. Render it out here however you like */}
+      <h1>{props.slogan}</h1>
     </div>
   );
 }
