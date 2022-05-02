9474f32443684b6de108bbdc8466b7d7699af3f0
THOMAS HARDING <hardingt604@gmail.com>
2022-05-02 13:15:56 -0600

    add cityname, dropdowns, and slogan copmonents to app.js

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
   // you'll need to track a city name, which starts as the city name of your choice.
   const [cityName, setCityName] = useState('Billiamshire');
   // finally, you'll need an array of slogans, which could start out as ['The City of Excellence'] for example
-  const [sloganArray, setSloganArray] = useState(['Sparkling Fields of Grey']);
+  const [slogans, setSlogans] = useState(['Sparkling Fields of Grey']);
 
   return (
     <div className="App">
@@ -29,15 +29,21 @@ function App() {
       </h1>
       <div className='bottom'>
         {/* here, the CityNameInput component takes in the setCityName state handler function */}
+        <CityNameInput setCityName={setCityName} />
         <section className='dropdowns'>
           {/* 
           render all three Dropdown components (WaterfrontDropdown, SkylineDropdown, CastleDropdown) here. 
           
           In each Dropdown, pass as props the state handler functions defined above, so that these child components can manipulate parent state 
           */}
+          <WaterfrontDropdown setWaterfrontId={setWaterfrontId} />
+          <SkylineDropdown setSkylineId={setSkylineId} />
+          <CastleDropdown setCastleId={setCastleId} />
         </section>
         {/* here, the SloganForm component takes in the setSlogans state handler function and the slogans array that live in state */}
+        <SloganForm setSlogans={setSlogans} slogans={slogans} />
         {/* here, the SloganList component takes the array of slogans that lives in state */}
+        <SloganList slogans={slogans} />
 
       </div>
     </div>
