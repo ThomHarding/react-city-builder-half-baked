16ee3b0968da536860c15053cab2685f1adfd334
THOMAS HARDING <hardingt604@gmail.com>
2022-05-02 13:07:25 -0600

    add initial state, renders City component

--- src/App.js
+++ src/App.js
@@ -11,15 +11,21 @@ import City from './City';
 function App() {
   // track some state here.
   // You'll need to keep track of a skylineId, waterfrontId, and castleId. All these start out as 1
+  const [skylineID, setSkylineId] = useState(1);
+  const [waterfrontID, setWaterfrontId] = useState(1);
+  const [castleID, setCastleId] = useState(1);
   // you'll need to track a city name, which starts as the city name of your choice.
+  const [cityName, setCityName] = useState('Billiamshire');
   // finally, you'll need an array of slogans, which could start out as ['The City of Excellence'] for example
+  const [sloganArray, setSloganArray] = useState(['Sparkling Fields of Grey']);
 
   return (
     <div className="App">
       {/* here, the City component takes in skylineId, waterfrontId, castleId as props. It'll use those ids to render pictures correctly. */}
+      <City skylineID={skylineID} waterfrontID={waterfrontID} castleID={castleID} />
       <h1>
         {/* dynamically update the city name here using state */}
-        Welcome to beautiful Portland!
+        Welcome to beautiful {cityName}!
       </h1>
       <div className='bottom'>
         {/* here, the CityNameInput component takes in the setCityName state handler function */}
