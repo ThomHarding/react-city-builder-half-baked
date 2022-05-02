e7882e395c420fc5d8ec21ae064e86b80c68b0d9
THOMAS HARDING <hardingt604@gmail.com>
2022-05-02 13:51:27 -0600

    add castledropdown.js

--- src/City.js
+++ src/City.js
@@ -3,8 +3,8 @@ import React from 'react';
 export default function City(props) {
   return (
     <div className="city">
-      {/* this component should take 3 props: waterfrontId, skylineId, and castlId */}
-      {/* use these 3 ids in img tags load approriate images that live in the public directory */}
+      {/* this component should take 3 props: waterfrontId, skylineId, and castleId */}
+      {/* use these 3 ids in img tags load appropriate images that live in the public directory */}
     </div>
   );
 }
